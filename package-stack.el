17e5ec534c2abc994d096c09396fd2417968a66e
Ailrun <jjc9310@gmail.com>
2017-02-07 23:26:06 +0900

    Add standardizer

--- package-stack.el
+++ package-stack.el
@@ -5,7 +5,7 @@
 ;; Author: Junyoung Clare Jang <jjc9310@gmail.com>
 ;; Maintainer: Junyoung Clare Jang <jjc9310@gmail.com>
 ;; Created: 31 Jan 2017
-;; Version: 0.1
+;; Version: 0.0.1
 ;; Package-Requires: (dash)
 ;; Keywords: dotemacs startup speed config package
 ;; URL: https://github.com/ailrun/package-stack
@@ -26,32 +26,13 @@
 ;;; Code:
 
 (require 'dash)
+(require 'package-stack-log)
+
+(defconst package-stack/github-url "https://github.com/ailrun/package-stack")
 
 (defgroup package-stack nil
   "Settings for `package-stack'."
   :group 'emacs)
-
-(defconst package-stack/github-url "https://github.com/ailrun/package-stack")
-
-(defconst package-stack/no-log 0)
-(defconst package-stack/load-log 1)
-(defconst package-stack/verbose-log 2)
-(defconst package-stack/debug-log 3)
-
-(defcustom package-stack/log-level package-stack/debug-log
-  "Setting for log level of package-stack.
-
-Lowest log level is ‘No Log’, and in ascending order,
-next levels are ‘Load Log’, ‘Macro Log’, and ‘Debug Log’."
-  :type '(choice (const :tag "No Log"
-                        package-stack/no-log)
-                 (const :tag "Load Log"
-                        package-stack/load-log)
-                 (const :tag "Verbose Log"
-                        package-stack/verbose-log)
-                 (const :tag "Debug Log"
-                        package-stack/debug-log))
-  :group 'package-stack)
 
 (defcustom package-stack/keywords
   '(:ignored
@@ -73,136 +54,160 @@ just ignored with ‘package-stack’ itself."
   :type '(repeat symbol)
   :group 'package-stack)
 
-(defmacro package-stack/log-caller-name ()
-  "Get the name of log caller at expansion time."
-  (symbol-name
-   (cadr
-    (cadr
-     (--first (ignore-errors
-                (equal (car it) 'defalias))
-              (reverse (let ((frames-args)
-                             (frame)
-                             (index 10))
-                         (while (setq frame (backtrace-frame index))
-                           (when (car frame)
-                             (push (nth 2 frame) frames-args))
-                           (setq index (1+ index)))
-                         frames-args)))))))
 
-(defmacro package-stack/log (level
-                             msg-format &rest args)
-  "If LEVEL is less than ‘package-stack/log-level’, write log.
-
-Write log using MSG-FORMAT and ARGS when LEVEL is
-less than ‘package-stack/log-level’."
-  (declare (indent 1))
-  `(unless (= package-stack/no-log ,level)
-     (when (<= ,level package-stack/log-level)
-       (message "%s"
-                (concat
-                 (when (<= package-stack/load-log ,level)
-                   (format "[%s] "
-                           (format-time-string "%Y-%m-%d, %T")))
-                 (when (<= package-stack/debug-log ,level)
-                   (format "%s --- " (package-stack/log-caller-name)))
-                 "\""
-                 (format ,msg-format ,@args)
-                 "\"")))))
-
-(defun package-stack/break (pred ls)
-  "In point of predicate PRED, split list LS into unsatisfied prefix and rest."
-  (let ((res nil)
-        (cont t))
-    (while (and ls cont)
-      (if (funcall pred (car ls))
-          (setq cont nil)
-        (setq res (cons (car ls) res))
-        (setq ls (cdr ls))))
-    (cons (nreverse res) ls)))
 
 (defun package-stack/settings-to-keypl (name-sym settings)
   "NAME-SYM SETTINGS."
   (declare (indent 1))
-  (if settings
+  (let (result)
+    (while settings
       (let* ((keyword (car settings))
-             (pair (package-stack/break #'keywordp (cdr settings)))
-             (keyargs (car pair))
-             (tail (cdr pair)))
-        (cons (cons keyword keyargs)
-              (package-stack/settings-to-keypl name-sym tail)))
-    nil))
+             (keyargstail (--split-with (not (keywordp it))
+                                        (cdr settings)))
+             (keyargs (car keyargstail))
+             (tail (cadr keyargstail)))
 
-(defun package-stack/validate-keypl (name-sym keypl)
-  "NAME-SYM KEYPL."
+        (!cons (cons keyword keyargs) result)
+        (setq settings tail)))
+    result))
+
+(defun package-stack/standardize-keypl (name-sym keypl)
+  "Standardizer of NAME-SYM for all KEYPL."
   (declare (indent 1))
-  (package-stack/log package-stack/debug-log
-    "")
-  (when keypl
-    (let* ((keyword (caar keypl))
-           (args (cdar keypl))
-           (validator (intern (concat "package-stack/validate-"
-                                      (symbol-name keyword)))))
-      (cond ((eq keyword :ignored)
-             (error (concat
-                     "Unchecked :ignored was found in %s.  "
-                     "Please report this via %s")
-                    (symbol-name name-sym)
-                    package-stack/github-url))
-            ((functionp validator)
-             (funcall validator name-sym args))
-            (t
-             (error "No validator for %s was found"
-                    keyword))))))
+  (package-stack/debug-log
+   "")
+
+  (--map
+   (let* ((keyword (car it))
+          (args (cdr it))
+          (validator (intern (concat "package-stack/validate-"
+                                     (symbol-name keyword))))
+          (standardizer (intern (concat "package-stack/standardize-"
+                                        (symbol-name keyword))))
+
+          (validity (progn
+                      (cond ((eq keyword :ignored)
+                             (error (concat
+                                     "Unchecked :ignored was found in %s.  "
+                                     "Please report this via %s")
+                                    (symbol-name name-sym)
+                                    package-stack/github-url))
+                            ((functionp validator)
+                             (package-stack/debug-log
+                              "Validator=%s" validator)
+                             (funcall validator name-sym args))
+                            (t
+                             (error "No validator for %s was found"
+                                         keyword)))))
+
+          (standard-args (progn
+                           (cond ((null validity)
+                                  (error "Keyword %s with invalid args %s"
+                                         args
+                                         keyword))
+                                 ((functionp standardizer)
+                                  (package-stack/debug-log
+                                   "Standardizer=%s" standardizer)
+                                  (funcall standardizer name-sym args))
+                                 (t
+                                  (error "No standardizer for %s was found"
+                                         keyword))))))
+
+     (package-stack/verbose-log
+      "Standard Args of %s=%s" keyword standard-args)
+     (cons keyword standard-args))
+   keypl))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Commons for keywords
 ;;
 
-(defun package-stack/validate-no-args (_ args)
-  "Common validator for no ARGS keywords."
+(defun package-stack/validate-no-args (_name-sym args)
+  "Common validator for keywords with no ARGS.
+
+Keywords with no ARGS requires empty ARGS.
+
+1. :keyword"
   (declare (indent 1))
-  (package-stack/log package-stack/debug-log
-    "ARGS=%S" args)
+  (package-stack/debug-log
+   "ARGS=%S" args)
   (null args))
+
+(defun package-stack/standardize-no-args (_name-sym args)
+  "Common standardizer for keywords with no ARGS.
+
+Standard keywords with no ARGS use nothing.
+
+1. :keyword"
+  (declare (indent 1))
+  (package-stack/debug-log
+   "ARGS=%S" args)
+  nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; :now keyword
 ;;
 
-(defalias 'package-stack/validate-:now 'package-stack/validate-no-args)
+(defalias 'package-stack/validate-:now
+  'package-stack/validate-no-args)
+
+(defalias 'package-stack/standardize-:now
+  'package-stack/standardize-no-args)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; :on-start keyword
 ;;
 
-(defalias 'package-stack/validate-:on-start 'package-stack/validate-no-args)
+(defalias 'package-stack/validate-:on-start
+  'package-stack/validate-no-args)
+
+(defalias 'package-stack/standardize-:on-start
+  'package-stack/standardize-no-args)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; :first keyword
 ;;
 
-(defun package-stack/validate-:first (_ args)
-  "Validator for ARGS of ‘:first’ keyword.
+(defun package-stack/validate-:first (_name-sym args)
+  "Validatore for ‘:first’ keyword with ARGS.
 
-‘:first’ keyword requires S-Exps or a S-Exp."
+‘:first’ keyword requires S-Exps or a list of S-Exp.
+
+1. ‘:first’ S-EXP ...
+2. ‘:first’ (S-EXP ...)"
   (declare (indent 1))
-  (package-stack/log package-stack/debug-log
-    "ARGS=%S" args)
+  (package-stack/debug-log
+   "ARGS=%S" args)
   (listp (car args)))
+
+(defun package-stack/standardize-:first (_name-sym args)
+  "Standardizer for ARGS of ‘:first’ keyword.
+
+Standard ‘:first’ keyword use S-Exps.
+
+1. ‘:first’ S-EXP ..."
+  (declare (indent 1))
+  (package-stack/debug-log
+   "ARGS=%S" args)
+  (if (cdr args)
+      args
+    (car args)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; :modes keyword
 ;;
 
-(defun package-stack/validate-:mode (_ args)
-  "Validator for ARGS of ‘:mode’ keyword.
+(defun package-stack/validate-:mode (_name-sym args)
+  "Validator for ‘:mode’ keyword with ARGS.
 
-‘:mode’ keyword requires a list of string/symbol pair"
+‘:mode’ keyword requires a list of string/symbol pair.
+
+1. ‘:mode’ ((STRING . SYMBOL) ...)"
   (declare (indent 1))
   (and (listp args)
        (--all? (and (consp it)
@@ -210,12 +215,84 @@ less than ‘package-stack/log-level’."
                     (symbolp (cdr it)))
                args)))
 
+(defun package-stack/standardize-:mode (_name-sym args)
+  "Standardizer for ‘:mode’ keyword with ARGS.
+
+Standard ‘:mode’ keyword use a list of string/symbol pair.
+
+1. ‘:mode’ ((STRING . SYMBOL) ...)"
+  (declare (indent 1))
+  (package-stack/debug-log
+   "ARGS=%S" args)
+  args)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; :interpreters keyword
+;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; :commands keyword
+;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; :before-deps keyword
+;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; :deps keyword
+;;
+
+(defun package-stack/validate-:deps (_name-sym args)
+  "Validator for ARGS of ‘:deps’ keyword.
+
+‘:deps’ keyword requires a list of, strings or symbols.
+
+1. ‘:deps’ (SYMBOL-OR-STRING ...)"
+  (declare (indent 1))
+  (package-stack/debug-log
+   "ARGS=%S" args)
+  (and (listp (car args))
+       (--all? (or (stringp it)
+                   (symbolp it))
+               (car args))))
+
+(defun package-stack/standardize-:deps (_name-sym args)
+  "Validator for ARGS of ‘:deps’ keyword.
+
+Standard ‘:deps’ keyword use a list of symbols.
+
+1. ‘:deps’ (SYMBOL ...)"
+  (declare (indent 1))
+  (package-stack/debug-log
+   "ARGS=%S" args)
+  (--map (if (stringp it)
+             (intern it)
+           it)
+         args))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; :before-load keyword
+;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; :after-load keyword
+;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; :last keyword
+;;
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; The package-stack macro
 ;;
-
-;; TEST: (package-stack 'this :first ((cons 1 2) (cons 2 3)))
 
 (defmacro package-stack (package-name &rest settings)
   "Define an isolated package configuration for PACKAGE-NAME using SETTINGS.
@@ -247,9 +324,14 @@ Keywords in order they resolved.
                          (intern package-name)
                        package-name))
            (keypl (package-stack/settings-to-keypl name-sym settings))
-           (valid (package-stack/validate-keypl name-sym keypl)))
-      valid)))
+           (valid (package-stack/standardize-keypl name-sym keypl)))
+      t)))
 
+;; TEST:
+(package-stack this
+  :now
+  :first ((cons 1 2) (cons 2 3))
+  :deps (hi bi))
 
 (provide 'package-stack)
 
